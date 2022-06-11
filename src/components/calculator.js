9a11e5f2b450402d3a391a68110dc5b931636445
EduardoRdz007 <edurdz010@gmail.com>
2022-06-11 14:37:56 -0500

    feact: The buttons for its operation were added and the mathematical functions are added

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,4 @@
-// Imports.
+
 import React from 'react';
 import CalculatorTitle from './calculatorTitle.js';
 import OutputScreen from './outputScreen.js';
@@ -8,7 +8,6 @@ class Calculator extends React.Component {
 
   constructor() {
   super();
-  // set our default state
   this.state = {
     question: '',
     answer: ''
@@ -58,17 +57,12 @@ class Calculator extends React.Component {
     );
   }
 
-  // our method to handle all click events from our buttons
   handleClick(event){
 
-    // get the value from the target element (button)
     const value = event.target.value;
 
     switch (value) {
       case '=': {
-        // if it's an equal sign, use the eval module
-        // to evaluate the question ,convert the answer
-        // (in number) to String
     		if(this.state.question!=='')
         {
     			var ans='';
@@ -88,29 +82,3 @@ class Calculator extends React.Component {
     			break;
     		}
       }
-      case 'Clear': {
-        // if it's the Clears sign, just clean our
-        // question and answer in the state
-        this.setState({ question: '', answer: '' });
-        break;
-      }
-
-	  case 'Delete': {
-	    var str = this.state.question;
-  		str = str.substr(0,str.length-1);
-  		this.setState({question: str});
-  		break;
-	  }
-
-    default: {
-        // for every other commmand, update the answer in the state
-        this.setState({ question: this.state.question += value})
-        break;
-      }
-    }
-  }
-
-}
-
-// Export Calculator Component.
-export default Calculator;
